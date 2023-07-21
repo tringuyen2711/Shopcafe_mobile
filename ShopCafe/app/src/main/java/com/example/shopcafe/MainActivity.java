32715adb892820e9465f19ab5f2ac7306caf9152
tringuyen123 <95064327+tringuyen2711@users.noreply.github.com>
2023-07-21 18:29:15 +0700

    page1( function to page 1 is uncomplete)

--- ShopCafe/app/src/main/java/com/example/shopcafe/MainActivity.java
+++ ShopCafe/app/src/main/java/com/example/shopcafe/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.shopcafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class MainActivity extends AppCompatActivity {
+    Timer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // Activity to page 2
+            }
+        },5000);
     }
 }
